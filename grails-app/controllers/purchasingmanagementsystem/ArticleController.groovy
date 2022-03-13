1ace55c7f720f3bab61208976d22e64e37dd400f
Julio Sanchez <juliosanchez692@gmail.com>
2022-03-13 01:10:47 -0400

    Add USER and ADMIN roles to controllers.

--- grails-app/controllers/purchasingmanagementsystem/ArticleController.groovy
+++ grails-app/controllers/purchasingmanagementsystem/ArticleController.groovy
@@ -2,7 +2,9 @@ package purchasingmanagementsystem
 
 import grails.validation.ValidationException
 import static org.springframework.http.HttpStatus.*
+import grails.plugin.springsecurity.annotation.Secured
 
+@Secured(["ROLE_ADMIN", "ROLE_USER"])
 class ArticleController {
 
     ArticleService articleService
